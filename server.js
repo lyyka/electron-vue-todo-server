a22c47fa46e22bff09bf64f421bf86c6b2af4124
Luka Robajac <luka.robajac@gmail.com>
2020-10-24 00:26:11 +0200

    MongoDB url is now part of the .env

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv')
 dotenv.config()
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb://127.0.0.1:27017/todo', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true});
 
 // Config the app
 const express = require('express')
